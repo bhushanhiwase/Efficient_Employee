ccba57707eae75e612972df203032ea6aefb1c1a
Bhushan Hiwase <bhushiwase@gmail.com>
2020-05-21 11:59:16 -0700

    Update Efficient_Employee_code.py

--- Efficient_Employee_code.py
+++ Efficient_Employee_code.py
@@ -5,11 +5,11 @@
 
 # The code is as follows:
 
-file1 = open("d:/Efficient Employee/word.csv", "r")                                # Opens 1st CSV file
+file1 = open("d:/Efficient Employee/word.csv", "r")               # Opens 1st CSV file/ you can also use "with open" to open the file
 lst = file1.readlines()
 file1.close()
 
-file2 = open("d:/Efficient Employee/word2.csv", "r")                                # Opens 2nd CSV file
+file2 = open("d:/Efficient Employee/word2.csv", "r")              # Opens 2nd CSV file
 lst1 = file2.readlines()
 file2.close()
 
@@ -20,16 +20,16 @@ for i in range(1, len(lst)):                                      # Iterate or l
 
 s_list = sorted(ratio, key=lambda t: t[0])                        # Sort the ratio list with the ascending order of the ratio
 
-result = []                                                          # result list contains final ratio info
+result = []                                                       # result list contains final ratio info
 for i in range(1, len(lst)):
     z = lst[i].split(",")
     if z[0] == s_list[-1][1]:                                     # finds the name of the fastest worker from sorted list
-        result.extend(z)							                  # adds the speed of that worker into the ans list
+        result.extend(z)							              # adds the speed of that worker into the result list
 
 for j in range(1, len(lst1)):
     k = lst1[j].split(",")
     if k[0] == s_list[-1][1]:                                    # finds the name of the fastest worker from sorted list
-        result.extend((k[1],k[2]))							         # adds the speed of that worker into the ans list
+        result.extend((k[1],k[2]))							     # adds the speed of that worker into the result list
 
 print("{0} is the most efficient worker, {0} has done {1} units of Work , using {2} unit resources, worker's Salary is: {3}K and gender is : {4}".format(result[0], result[1], result[2], result[3], result[4]))
 
